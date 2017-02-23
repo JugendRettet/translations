b4aec62c62e77b980af251d026b75c15bace5e79
alexh-name <1@alexh.name>
2017-02-23 20:40:20 +0100

    make.sh: choose correct branch to build website

--- make.sh
+++ make.sh
@@ -9,8 +9,10 @@ if [ -f "${website_dir}"/wswsh ]; then
   printf "%s\n" "Website's dir seems to contain wswsh. Good."
   printf "%s\n" "Syncing translations into it..."
   rsync -qaP --del --exclude='.git' "${our_dir}" "${website_dir}"/
-  printf "%s\n" "Building new website..."
   cd "${website_dir}"
+  printf "%s\n" "Switching to branch ${BRANCH}..."
+  git checkout "${BRANCH}"
+  printf "%s\n" "Building new website..."
   URL="${URL}" WWW_DIR="${WWW_DIR}" ./make.sh
 else
   printf "%s\n" "Website's dir doesn't seem to contain wswsh. Aborted."
